58b41ab4e7b205bbe4a482e32110e36b57fa3af6
Peter Preeper <ppreeper@gmail.com>
2018-03-22 11:11:48 -0600

    add randomness

--- mathquiz.go
+++ mathquiz.go
@@ -3,29 +3,56 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/ppreeper/stringpad"
 )
 
+var randSource = rand.NewSource(time.Now().UnixNano())
+
 func main() {
 	fmt.Println("Welcome to Math Quiz")
-	question("add", 5)
+	for i := 0; i < 5; i++ {
+		question("add", 1)
+	}
 }
 
 func question(symbol string, level int) {
-	firstNumber := 11111
-	secondNumber := 11111
-	answer := firstNumber * secondNumber
-	firstString := stringpad.RJustLen(strconv.Itoa(firstNumber), level+2)
-	secondString := stringpad.RJustLen("x "+strconv.Itoa(secondNumber), level+2)
-	totalLine := stringpad.LPadLen("-", "-", level+2)
+	var fnum int
+	var snum int
+	var answer int
+	var anslen int
+	var firstString string
+	var secondString string
+
+	if level > 1 {
+		fnum, _ = strconv.Atoi(stringpad.RPadLen("1", "0", level))
+		snum, _ = strconv.Atoi(stringpad.LPadLen("", "9", level))
+	} else {
+		fnum, _ = strconv.Atoi(stringpad.RPadLen("0", "0", level))
+		snum, _ = strconv.Atoi(stringpad.LPadLen("", "9", level))
+	}
+
+	var r = rand.New(randSource)
+	firstNumber := r.Intn(snum-fnum) + fnum
+	secondNumber := r.Intn(snum-fnum) + fnum
+	switch symbol {
+	case "add":
+		answer = firstNumber + secondNumber
+		anslen = len(strconv.Itoa(snum+snum)) + 2
+		firstString = stringpad.RJustLen(strconv.Itoa(firstNumber), anslen)
+		secondString = stringpad.RJustLen("+ "+strconv.Itoa(secondNumber), anslen)
+
+	}
+	totalLine := stringpad.LPadLen("-", "-", anslen)
 	fmt.Printf("%s\n", firstString)
 	fmt.Printf("%s\n", secondString)
 	fmt.Printf("%s\n", totalLine)
-	totalString := stringpad.RJustLen(strconv.Itoa(firstNumber+secondNumber), level+2)
+	totalString := stringpad.RJustLen(strconv.Itoa(answer), anslen)
 
 	reader := bufio.NewReader(os.Stdin)
 	ans, _ := reader.ReadString('\n')
@@ -35,9 +62,9 @@ func question(symbol string, level int) {
 		panic(err)
 	}
 	if ansInt == answer {
-		fmt.Printf("Correct\n")
+		fmt.Printf("\rCorrect\n")
 	} else {
-		fmt.Printf("Nope: %s\n", totalString)
+		fmt.Printf("\rNope: %s\n", totalString)
 	}
 
 }
